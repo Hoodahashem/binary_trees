b6a0a196742812c33eb2ff8e73b363f43b121fbe
Hoodahashem <mahmoudhashem335@gmail.com>
2024-01-04 01:57:25 +0200

    3rd task

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,31 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_right - functions insert in the right
+ * @parent: the root
+ * @value: the value
+ * Return: new node
+*/
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *right;
+
+	if (!parent)
+	{
+		return (NULL);
+	}
+	right = binary_tree_node(parent, value);
+
+	if (!right)
+	{
+		return (NULL);
+	}
+	if (parent->right)
+	{
+		right->right = parent->right;
+		parent->right->parent = right;
+	}
+	parent->right = right;
+
+	return (right);
+}
