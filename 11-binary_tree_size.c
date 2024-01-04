2e1f3ef897f08883aa85f0269d85609bb483b1e1
Hoodahashem <mahmoudhashem335@gmail.com>
2024-01-04 16:14:32 +0200

    12 task is done

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -9,8 +9,14 @@ size_t binary_tree_size(const binary_tree_t *tree)
 {
 	size_t sizeR, sizeL;
 
-	if (tree == NULL || )
+	if (tree == NULL)
 	{
-		return (0)
+		return (0);
+	}
+	else
+	{
+		sizeL = binary_tree_size(tree->left);
+		sizeR = binary_tree_size(tree->right);
+		return (sizeR + sizeL + 1);
 	}
 }
