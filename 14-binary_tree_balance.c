5ca7ae9dd25d6fa9dc292a9f3cbe16eb4fa02f12
Hoodahashem <mahmoudhashem335@gmail.com>
2024-01-04 19:40:38 +0200

    the task number 15 is done

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_balance - culculate the BF
+ * @tree: the root
+ * Return: the result
+*/
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	size_t right = binary_tree_height(tree->right);
+	size_t left = binary_tree_height(tree->left);
+
+	return ((int)(left - right));
+}
