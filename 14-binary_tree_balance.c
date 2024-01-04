b3f5721b51bc107f99096eeb8c4d4dc92d9e5352
Hoodahashem <mahmoudhashem335@gmail.com>
2024-01-04 19:50:35 +0200

    fixing a bug

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,5 +1,29 @@
 #include "binary_trees.h"
 /**
+ * heightagain - function that calc the height
+ * @tree:the root
+ * Return: an int
+*/
+int heightagain(const binary_tree_t *tree)
+{
+	int left, right;
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	else if (tree->left == NULL && tree->right == NULL)
+	{
+		return (0);
+	}
+	else
+	{
+		left = heightagain(tree->left);
+		right = heightagain(tree->right);
+		return ((left >= right) ? left + 1 : right + 1);
+	}
+}
+/**
  * binary_tree_balance - culculate the BF
  * @tree: the root
  * Return: the result
@@ -12,9 +36,9 @@ int binary_tree_balance(const binary_tree_t *tree)
 	}
 	else
 	{
-		size_t right = binary_tree_height(tree->right);
-		size_t left = binary_tree_height(tree->left);
+		int right = heightagain(tree->right);
+		int left = heightagain(tree->left);
 
-		return ((int)(left - right));
+		return (left - right);
 	}
 }
