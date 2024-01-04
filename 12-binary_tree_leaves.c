6eed2e554f9797e370dd49a7a780344d4ba64b02
Hoodahashem <mahmoudhashem335@gmail.com>
2024-01-04 17:11:00 +0200

    fixing the task number 13

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -7,7 +7,7 @@
 */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-	if (tree == NULL)
+	if (tree == NULL || tree->left == NULL && tree->right)
 	{
 		return (0);
 	}
---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,11 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_nodes - counts the nodes with at least 1 child
+ * @tree: the root
+ * Return: how many nodes
+*/
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	
+}
