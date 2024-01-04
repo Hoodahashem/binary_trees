52745e6112c11f1747df11bffc7cfbfd41a5c566
Hoodahashem <mahmoudhashem335@gmail.com>
2024-01-04 19:44:07 +0200

    again

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -6,8 +6,15 @@
 */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	size_t right = binary_tree_height(tree->right);
-	size_t left = binary_tree_height(tree->left);
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	else
+	{
+		size_t right = binary_tree_height(tree->right);
+		size_t left = binary_tree_height(tree->left);
 
-	return ((int)(left - right));
+		return ((int)(left - right));
+	}
 }
